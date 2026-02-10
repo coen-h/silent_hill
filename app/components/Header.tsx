521025d6e1bdbfef405c189de6b9a04cc4370fab
coen-h <105825347+coen-h@users.noreply.github.com>
2026-02-10 18:02:55 +1300

    commit header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   };
 
   return (
-    <motion.div className="fixed top-0 z-50 w-full px-2 flex justify-center">
+    <motion.div className="fixed top-0 z-30 w-full px-2 flex justify-center">
     <motion.div animate={{ opacity: 1, filter: "blur(0px)" }} transition={{ duration: 0.5 }} className="flex border-x border-b border-neutral-800 bg-black/25 backdrop-blur rounded-b-xl justify-between items-center top-0 z-50 py-2 px-3 max-w-[1600px] w-full opacity-0 blur-xs">
       <button className="hover:opacity-50 transition-opacity cursor-pointer" onClick={() => scrollTo('#hero')}>
         <img className="h-10" src="https://companieslogo.com/img/orig/6BO.SG.D-68bbfc94.png?t=1720244490" />
