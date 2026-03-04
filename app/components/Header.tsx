9aa5b1d9ca45b818c59f1a0a99c1845f81f92236
Coen <105825347+coen-h@users.noreply.github.com>
2026-03-04 20:59:59 +1300

    imp

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -16,12 +16,12 @@ export default function Header(Odata) {
 
   return (
     <motion.div className="fixed top-0 z-30 w-full px-2 flex justify-center">
-    <motion.div animate={{ opacity: 1, filter: "blur(0px)" }} transition={{ duration: 0.5 }} className="flex border-x border-b border-neutral-800 bg-black/25 backdrop-blur rounded-b-xl justify-between items-center top-0 z-50 py-2 px-3 max-w-[1600px] w-full opacity-0 blur-xs">
+    <motion.div animate={{ opacity: 1, filter: "blur(0px)" }} transition={{ duration: 0.5 }} className="flex border-x border-b border-neutral-800 bg-black/25 backdrop-blur-lg rounded-b-xl justify-between items-center top-0 z-50 py-2 px-3 max-w-[1600px] w-full opacity-0 blur-xs">
       <button className="hover:opacity-50 transition-opacity cursor-pointer" onClick={() => scrollTo('#hero')}>
         <img className="h-10" src={data.img} />
       </button>
       <div className="flex gap-4">
-        <motion.button transition={{ duration: 0.6 }} initial={{ opacity: 0, filter: "blur(8px)" }} whileInView={{ opacity: 1, filter: "blur(0px)" }} onClick={() => scrollTo('#concept')} className="hover:text-neutral-500 transition-colors cursor-pointer max-sm:text-sm">Concept</motion.button>
+        {/* <motion.button transition={{ duration: 0.6 }} initial={{ opacity: 0, filter: "blur(8px)" }} whileInView={{ opacity: 1, filter: "blur(0px)" }} onClick={() => scrollTo('#concept')} className="hover:text-neutral-500 transition-colors cursor-pointer max-sm:text-sm">Concept</motion.button> */}
         <motion.button transition={{ duration: 0.6, delay: 0.2 }} initial={{ opacity: 0, filter: "blur(8px)" }} whileInView={{ opacity: 1, filter: "blur(0px)" }} onClick={() => scrollTo('#characters')} className="hover:text-neutral-500 transition-colors cursor-pointer max-sm:text-sm">Characters</motion.button>
         <motion.button transition={{ duration: 0.6, delay: 0.4 }} initial={{ opacity: 0, filter: "blur(8px)" }} whileInView={{ opacity: 1, filter: "blur(0px)" }} onClick={() => scrollTo('#story')} className="hover:text-neutral-500 transition-colors cursor-pointer max-sm:text-sm">Story</motion.button>
         <motion.button transition={{ duration: 0.6, delay: 0.6 }} initial={{ opacity: 0, filter: "blur(8px)" }} whileInView={{ opacity: 1, filter: "blur(0px)" }} onClick={() => scrollTo('#gallery')} className="hover:text-neutral-500 transition-colors cursor-pointer max-sm:text-sm">Gallery</motion.button>
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Footer from '@/app/components/Footer';
 
 const sites = [
   '/sh1.json',
-  '/sh2.json',
+  '/sh.json',
   '/sh3.json',
   '/sh4.json'
 ]
@@ -35,15 +35,15 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center">
-      <div className='fixed flex items-center right-1 top-1 h-6 w-24 gap-1 px-0.5 bg-black/90 outline outline-white/20 rounded-lg z-50'>
+      {/* <div className='fixed flex items-center right-1 top-1 h-6 w-24 gap-1 px-0.5 bg-black/90 outline outline-white/20 rounded-lg z-50'>
         <div onClick={e => setSelectedSite(0)} className='h-5 w-5 bg-white/40 rounded-lg cursor-pointer'></div>
         <div onClick={e => setSelectedSite(1)} className='h-5 w-5 bg-white/40 rounded-lg cursor-pointer'></div>
         <div onClick={e => setSelectedSite(2)} className='h-5 w-5 bg-white/40 rounded-lg cursor-pointer'></div>
         <div onClick={e => setSelectedSite(3)} className='h-5 w-5 bg-white/40 rounded-lg cursor-pointer'></div>
-      </div>
+      </div> */}
       <Header data={data.header} />
       <Hero data={data.hero} />
-      <Concept data={data.concept} />
+      {/* <Concept data={data.concept} /> */}
       <Characters data={data.characters} />
       <Story data={data.story} />
       <Gallery data={data.gallery} />
